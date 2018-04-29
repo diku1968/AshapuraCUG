75efb162934e6e395b25dae5f9f8619d75741497
diku1968 <diku1968@gmail.com>
2018-04-29 14:22:40 +0530

    Your Awesome Changes!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {HttpClient} from '@angular/common/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+   public myCuglists;
+  constructor(public http: HttpClient, public navCtrl: NavController) {
+  this.myCuglists = this.http.get("http://182.19.82.186:94/api/mobtrans");
   }
-
+  
 }
